3f0e2505cc99ce1d9ac76c8adcc94f55e281a1f6
dandumitriu33 <dandumitriu33@gmail.com>
2020-04-04 20:11:12 +0300

    Add solution - all tests successful

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,8 +2,69 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
-    @Override
-    public String toString() {
-        return "Implement me plz.. Pretty plz..";
+    int length;
+    int[] data = new int[length];
+
+    public DynamicIntArray(int x) {
+        length=x;
+        data = new int[length];
     }
+
+    public DynamicIntArray() {
+        length = 0;
+        data = new int[length];
+    }
+
+    public void add(int element) {
+        int[] result = new int[this.data.length+1];
+        for (int i=0; i<this.data.length; i++) {
+            result[i] = this.data[i];
+        }
+        result[result.length-1] = element;
+        this.data = new int[result.length];
+        for (int i=0; i<result.length; i++) {
+            this.data[i] = result[i];
+        }
+    }
+
+    public void remove(int element) {
+        int[] result = new int[this.data.length-1];
+        for (int i=0; i<element; i++) {
+            result[i] = this.data[i];
+        }
+        for (int i=element; i<result.length; i++) {
+            result[i] = this.data[i+1];
+        }
+        this.data = new int[result.length];
+        for (int i=0; i<result.length; i++) {
+            this.data[i] = result[i];
+        }
+    }
+
+    public void insert(int index, int value) {
+        int[] result = new int[this.data.length+1];
+        if (index>this.data.length) {
+            index = this.data.length;
+        }
+        for (int i=0; i<index; i++) {
+            result[i] = this.data[i];
+        }
+        result[index] = value;
+        for (int i=index; i<this.data.length; i++) {
+            result[i+1] = this.data[i];
+        }
+        this.data = new int[result.length];
+        for (int i=0; i<result.length; i++) {
+            this.data[i] = result[i];
+        }
+    }
+
+    public String toString() {
+        String result = "";
+        for (int i=0; i<this.data.length; i++) {
+            result += " " + this.data[i];
+        }
+        return result;
+    }
+
 }
--- src/main/java/com/codecool/dynamicArrayDojo/Program.java
+++ src/main/java/com/codecool/dynamicArrayDojo/Program.java
@@ -4,5 +4,12 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Do not run me, run the TESTS!");
+
+        DynamicIntArray obji = new DynamicIntArray(3);
+        obji.add(4);
+        for (int i=0; i<obji.data.length; i++) {
+            obji.data[i] = i;
+        }
+//        System.out.println(obji.data[2]);
     }
 }
